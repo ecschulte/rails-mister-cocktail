744d4d05866214b56b0bcd502144c83285d17b04
Eduard Schulte <ecschulte@gmx.de>
2020-08-13 16:54:09 +0200

    MR.Cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,7 +1,12 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :destroy]
 
   def index
     @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
   end
 
   def new
@@ -9,23 +14,26 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.new(strong_cocktail_params)
+    @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path, notice: 'Cocktail was successfully created.'
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
-
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
 
-  def strong_cocktail_params
-      params.require(:cocktail).permit(:name)
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,28 +1,34 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
+    # @ingredients = Ingredient.all
   end
 
   def create
+    @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new(strong_dose_params)
     @dose.cocktail = @cocktail
-
     if @dose.save
-        redirect_to cocktails_path(@restaurant)
+      redirect_to cocktail_path(@cocktail)
     else
-        render :new
+      render :new
     end
   end
 
   def destroy
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
-  private
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
 
-  def strong_dose_params
-    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
-
